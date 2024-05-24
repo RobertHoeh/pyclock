743f21e19ff3133e9e11de82b25d619bbf79e294
robertoman37 <robertoman7th@gmail.com>
2024-05-24 12:20:38 -0400

    Disable input by making inputScheme None

--- modules/moduleAbstract.py
+++ modules/moduleAbstract.py
@@ -7,31 +7,32 @@ class ModuleAbstract:
     def disp(self):
         ...
     def input(self):
-        match window.getch():
-            # executes if self.cursorMode == True
-            case curses.KEY_LEFT | curses.KEY_DOWN if self.cursorMode:
-                self.cursorPos.y += 1
-            case curses.KEY_RIGHT | curses.KEY_UP if self.cursorMode:
-                self.cursorPos.y -= 1
-            case curses.KEY_ENTER if self.cursorMode:
-                for i in self.buttons:
-                    if self.buttons.order == self.cursorPos.x:
-                        self.buttons[0][self.cursorPos.x].onClick(self)
-                        break
+        if self.inputScheme is not None:
+            match window.getch():
+                # executes if self.cursorMode == True
+                case curses.KEY_LEFT | curses.KEY_DOWN if self.cursorMode:
+                    self.cursorPos.y += 1
+                case curses.KEY_RIGHT | curses.KEY_UP if self.cursorMode:
+                    self.cursorPos.y -= 1
+                case curses.KEY_ENTER if self.cursorMode:
+                    for i in self.buttons:
+                        if self.buttons.order == self.cursorPos.x:
+                            self.buttons[0][self.cursorPos.x].onClick(self)
+                            break
 
-            # executes if self.cursorMode == False
-            case curses.KEY_RIGHT:
-                self.cursorPos.y += 1
-            case curses.KEY_LEFT:
-                self.cursorPos.y -= 1
-            case curses.KEY_DOWN:
-                self.cursorPos.x += 1
-            case curses.KEY_UP:
-                self.cursorPos.x -= 1
-            case curses.KEY_ENTER:
-                self.buttons[self.cursorPos.y][self.cursorPos.x].onClick(self)
-            case curses.KEY_EXIT:
-                return 1
+                # executes if self.cursorMode == False
+                case curses.KEY_RIGHT:
+                    self.cursorPos.y += 1
+                case curses.KEY_LEFT:
+                    self.cursorPos.y -= 1
+                case curses.KEY_DOWN:
+                    self.cursorPos.x += 1
+                case curses.KEY_UP:
+                    self.cursorPos.x -= 1
+                case curses.KEY_ENTER:
+                    self.buttons[self.cursorPos.y][self.cursorPos.x].onClick(self)
+                case curses.KEY_EXIT:
+                    return 1
     def getCenter(self):
         maxy, maxx = self.window.getmaxyx()
         return Pos(
