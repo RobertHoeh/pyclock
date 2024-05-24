11471eafc9521b16422ba94efd020ea43425bf54
robertoman37 <robertoman7th@gmail.com>
2024-05-24 12:26:35 -0400

    implement center function on Clock class

--- modules/clock.py
+++ modules/clock.py
@@ -1,20 +1,22 @@
 import time
 from definitions import Pos, DispInfo
+from modules.moduleAbstract import ModuleAbstract
 """provides a simple clock interface
 might implement customizable text size/ fancy text."""
 
-class Clock:
+class Clock(ModuleAbstract):
     def __init__(self, window, h12 = True):
         self.format = h12
         self.window = window
         self.pos = Pos(0,0)
         self.previous = self.pos
+        super().__init__(window, None)
 
     def disp(self):
         timestr = time.strftime("%I:%M:%S")
-        maxy, maxx = self.window.getmaxyx()
-        self.pos.y = maxy // 2
-        self.pos.x = maxx // 2 - len(timestr) // 2
+        center = super().getCenter()
+        self.pos.y = center.y
+        self.pos.x = center.x - len(timestr) // 2
         if self.previous != self.pos:
             self.window.clear()
         self.previous = self.pos
